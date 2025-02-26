7ff46c0d8d19d206463442d43e1a5231689238ed
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-02-26 08:37:46 +0000

    feat: swagger builder

--- nestia.config.ts
+++ nestia.config.ts
@@ -3,26 +3,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./src/app.module";
 
 export const NESTIA_CONFIG: INestiaConfig = {
-  /**
-   * Accessor of controller classes.
-   *
-   * You can specify it within two ways.
-   *
-   *   - Asynchronous function returning `INestApplication` instance
-   *   - Specify the path or directory of controller class files
-   */
-  input: async () => {
-    const app = await NestFactory.create(AppModule);
-    return app;
-  },
-
-  /**
-   * Building `swagger.json` is also possible.
-   *
-   * If not specified, you can't build the `swagger.json`.
-   */
+  input: () => NestFactory.create(AppModule),
+  
   swagger: {
-    output: "dist/swagger.json",
+    output: "packages/api/swagger.json",
+    servers: [
+      {
+        url: "http://localhost:37001",
+        description: "Local Server",
+      },
+    ],
+    beautify: true,
   },
 
   output: "src/api",
