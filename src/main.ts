fb0de833aeea5c43fe11de63519d1d49b8705acb
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-02-26 07:52:29 +0000

    feat: application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
-/**
- * 디렉토리 구조 저장용 temp file
- */
+import { INestApplication } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
+
+export class BackendApplication {
+  private application_?: INestApplication;
+
+  public async open(): Promise<void> {
+    // MOUNT CONTROLLERS
+    this.application_ = await NestFactory.create(AppModule, { logger: false });
+
+    // DO OPEN
+    this.application_.enableCors();
+    await this.application_.listen(3000, "0.0.0.0"); // TODO: 2025.02.26(luke0408) - Use config
+  }
+
+  public async close(): Promise<void> {
+    if (this.application_ === undefined) return;
+
+    // DO CLOSE
+    await this.application_.close();
+    delete this.application_;
+  }
+}
