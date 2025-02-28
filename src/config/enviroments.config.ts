3e13379e0be3440c0cde5ebc0af2f80f9c12c6e1
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-02-28 14:27:37 +0000

    feat: 전역 환경 변수 클래스 작성

---
+++ src/config/enviroments.config.ts
@@ -0,0 +1,70 @@
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+import { Singleton } from "tstl";
+import typia from "typia";
+
+/**
+ * Global enviroments for the application.
+ * 
+ * @author luke0408
+ * @see https://github.com/samchon/backend/blob/master/src/MyGlobal.ts
+ * @since 2025-02-28
+ */
+export class GlobalEnviroments {
+  public static testing: boolean = false;
+
+  public static readonly prisma: PrismaClient = new PrismaClient();
+
+  public static get env(): SystemEnvironments {
+    return environments.get();
+  }
+
+  /**
+   * @return The mode of the application.
+   *     - `local`: Local development mode.
+   *     - `dev`: Development mode.
+   *     - `prod`: Production mode.
+   */
+  public static get mode(): "local" | "dev" | "prod" {
+    return (modeWrapper.value ??= environments.get().MODE);
+  }
+
+  /**
+   * Set the mode of the application.
+   * 
+   * @param mode The mode of the application.
+   *     - `local`: Local development mode.
+   *     - `dev`: Development mode.
+   *     - `prod`: Production mode.
+   */
+  public static setMode(mode: typeof GlobalEnviroments.mode): void {
+    typia.assert<typeof mode>(mode);
+    modeWrapper.value = mode;
+  }
+}
+
+interface SystemEnvironments {
+  MODE: "local" | "dev" | "prod";
+  API_PORT: `${number}`;
+
+  POSTGRES_HOST: string;
+  POSTGRES_PORT: `${number}`;
+  POSTGRES_DATABASE: string;
+  POSTGRES_SCHEMA: string;
+  POSTGRES_USERNAME: string;
+  POSTGRES_USERNAME_READONLY: string;
+  POSTGRES_PASSWORD: string;
+}
+
+interface SystemMode {
+  value?: "local" | "dev" | "prod";
+}
+
+const modeWrapper: SystemMode = {};
+
+const environments = new Singleton(() => {
+  const env = dotenv.config();
+  dotenvExpand.expand(env);
+  return typia.assert<SystemEnvironments>(process.env);
+});
