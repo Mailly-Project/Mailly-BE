c82d11cfa9b6000286df6a8e6c1f8538f440c4e9
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-01 14:58:41 +0000

    docs: Error Type 관련 고민 주석

--- src/common/errors/index.ts
+++ src/common/errors/index.ts
@@ -1,5 +1,13 @@
 import { Merge } from "../types";
 
+/**
+ * Error Types
+ * 
+ * [고민] 
+ * - ERROR 타입의 code는 number가 맞는가?
+ *   - number가 맞다면 어떤 의미를 가지고 그 의미를 어떻게 정의할 것인가.
+ *   - string이 였다면 어떨까?
+ */
 export interface ERROR {
   type: string;
   result: false;
