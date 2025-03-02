28c9b698e1fd318573a76d66b5bdd4356e0b37ba
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-02 08:55:39 +0000

    feat: swagger ui set-up

--- src/common/middleware/logging.middleware.ts
+++ src/common/middleware/logging.middleware.ts
@@ -12,7 +12,7 @@ import { NextFunction } from 'express';
  */
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request & { now: number }, res: Response, next: NextFunction) {
+  use(req: Request & { now: number }, _res: Response, next: NextFunction) {
     req.now = Date.now();
     next();
   }
